97387b5fcff647b3834d0d35ba8efea7e7bcb726
nithinag <nithinag10@gmail.com>
2021-04-25 13:39:25 +0530

    torch model from MNIST dataset

--- main.py
+++ main.py
@@ -1,35 +1,86 @@
 import torch
-import torchvision
-from torch.utils.data import DataLoader, Dataset
-import numpy as np
-import math
-
 import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data.dataloader
+import   matplotlib.pyplot as plt
+
+device = torch.device('cpu')
+
+#HyperParameters
+inputlayer = 784
+hiddenlayer = 10
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+#loading MNIST Datasets
+train_dataset = torchvision.datasets.MNIST(root='./data' , train=True , transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root='./data' , train=False, transform=transforms.ToTensor(), download=False)
+
+train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100, shuffle=True)
+test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=100, shuffle=False)
+
+example = iter(train_dataloader)
+sample, labels = example.next()
 
 class NeuralNet(nn.Module):
-    def __init__(self, inputlayer, hiddenlayer):
-        super(NeuralNet, self).__init__()
-        self.linear1 = nn.Linear(inputlayer, hiddenlayer)
+    def __init__(self , inputlayer, hiddenlayer , num_classes):
+        super(NeuralNet , self).__init__()
+        self.linear1 = nn.Linear(inputlayer , hiddenlayer)
         self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(hiddenlayer, 1)
+        self.linear2 = nn.Linear(hiddenlayer , num_classes)
 
-    def forward(self, x):
-
+    def forward(self , x):
         out = self.linear1(x)
         out = self.relu(out)
         out = self.linear2(out)
+        return out
 
-        y_pred = torch.sigmoid(out)
+model = NeuralNet(inputlayer , hiddenlayer , num_classes)
 
-        return y_pred
+#loss and entrophy
+criterian = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters() , lr=learning_rate)
 
-model = NeuralNet(inputlayer=3, hiddenlayer=5)
-loss = nn.BCELoss()
+#interate
 
-Y = torch.tensor([2])
-y_pred = torch.tensor([[2,3,1]])
+n_total_steps = len(train_dataloader)
 
-print(loss(Y, y_pred))
+#training_loop
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_dataloader):
+        images = images.reshape(-1, 784).to(device)
+        labels = labels.to(device)
+        output = model(images)
+        loss = criterian(output , labels)
+
+        #backward_propogation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1)%100 == 0:
+            print(f'epoch {epoch+1}/{num_epochs} steps {i+1}/{n_total_steps} with loss {loss.item()}')
 
 
+#testing
 
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_dataloader:
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+
+        #torch.max returns value and index
+        _, prediction = torch.max(outputs, 1)
+        n_samples += labels.shape[0]
+        n_correct += (prediction == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+
+    print(f'Accuracy of the model is {acc}.')
